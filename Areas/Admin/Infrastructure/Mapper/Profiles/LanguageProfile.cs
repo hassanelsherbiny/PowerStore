7fb16bef7399248e9ffd50f1d05b110aa694cbba
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-01-26 02:16:03 +0200

    Implement product listing functionality

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/LanguageProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Localization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class LanguageProfile : Profile, IAutoMapperProfile
+    {
+        public LanguageProfile()
+        {
+            CreateMap<Domain.Localization.Language, LanguageModel>()
+                .ForMember(dest => dest.AvailableStores, mo => mo.Ignore())
+                .ForMember(dest => dest.AvailableCurrencies, mo => mo.Ignore())
+                .ForMember(dest => dest.SelectedStoreIds, mo => mo.Ignore())
+                .ForMember(dest => dest.FlagFileNames, mo => mo.Ignore());
+
+            CreateMap<LanguageModel, Domain.Localization.Language>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore())
+                .ForMember(dest => dest.Stores, mo => mo.MapFrom(x => x.SelectedStoreIds != null ? x.SelectedStoreIds.ToList() : new List<string>()));
+        }
+
+        public int Order => 0;
+    }
+}
