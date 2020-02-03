54b3770a956652d1a26920c01a3f17edd1c20472
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-02-03 13:38:02 +0200

    Optimize CSS for Faster Page Rendering

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,48 @@
+﻿using Grand.Framework.Mvc.Filters;
+using Grand.Services.Localization;
+using Grand.Web.Models.Common;
+using Grand.Web.Features.Models.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Controllers
+{
+    public partial class CountryController : BasePublicController
+    {
+        #region Fields
+
+        private readonly IMediator _mediator;
+        private readonly ILocalizationService _localizationService;
+
+        #endregion
+
+        #region Constructors
+
+        public CountryController(IMediator mediator, ILocalizationService localizationService)
+        {
+            _mediator = mediator;
+            _localizationService = localizationService;
+        }
+
+        #endregion
+
+        #region States / provinces
+
+        //available even when navigation is not allowed
+        [CheckAccessPublicStore(true)]
+        public virtual async Task<IActionResult> GetStatesByCountryId(string countryId, bool addSelectStateItem)
+        {
+            //this action method gets called via an ajax request
+            if (string.IsNullOrEmpty(countryId))
+            {
+                return Json(new List<StateProvinceModel>() { new StateProvinceModel { id = "", name = _localizationService.GetResource("Address.SelectState") } });
+            }
+            var model = await _mediator.Send(new GetStatesProvince() { CountryId = countryId, AddSelectStateItem = addSelectStateItem });
+            return Json(model);
+        }
+
+        #endregion
+    }
+}
