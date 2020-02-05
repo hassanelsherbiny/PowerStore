d3db12e2aaab222b9d3ea6b6f2a5c5dd3b858988
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-02-05 11:59:03 +0200

    Add sample product data

---
+++ Components/PollBlock.cs
@@ -0,0 +1,31 @@
+﻿using Grand.Framework.Components;
+using Grand.Web.Features.Models.Polls;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class PollBlockViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+
+        public PollBlockViewComponent(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string systemKeyword)
+        {
+            if (string.IsNullOrWhiteSpace(systemKeyword))
+                return Content("");
+
+            var model = await _mediator.Send(new GetPollBySystemName() { SystemName = systemKeyword });
+            if (model == null || string.IsNullOrWhiteSpace(model.Id))
+                return Content("");
+
+            return View(model);
+
+        }
+    }
+}
