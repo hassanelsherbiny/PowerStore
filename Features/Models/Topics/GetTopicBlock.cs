c970e9db78fda18a7d17373c7065b4d1f614cca5
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-02-06 01:13:03 +0200

    Create product details page

---
+++ Features/Models/Topics/GetTopicBlock.cs
@@ -0,0 +1,12 @@
+﻿using Grand.Web.Models.Topics;
+using MediatR;
+
+namespace Grand.Web.Features.Models.Topics
+{
+    public class GetTopicBlock : IRequest<TopicModel>
+    {
+        public string SystemName { get; set; }
+        public string TopicId { get; set; }
+        public string Password { get; set; } 
+    }
+}
