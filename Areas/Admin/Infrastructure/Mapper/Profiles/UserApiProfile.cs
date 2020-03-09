3562b9f149b928806de04c7acb9ad0dfcb4750ee
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-03-09 14:28:06 +0200

    Implement user logout functionality

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/UserApiProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Grand.Domain.Customers;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Customers;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class UserApiProfile : Profile, IAutoMapperProfile
+    {
+        public UserApiProfile()
+        {
+            CreateMap<UserApi, UserApiModel>()
+                .ForMember(dest => dest.Password, mo => mo.Ignore());
+            CreateMap<UserApiModel, UserApi>()
+                .ForMember(dest => dest.Password, mo => mo.Ignore())
+                .ForMember(dest => dest.Id, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
