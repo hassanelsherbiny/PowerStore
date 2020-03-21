ee0cfb7a719cbcb9c0a1e7cdf8f0a7880924e7dd
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-03-22 01:49:02 +0200

    Implement Asynchronous Loading for Non-Essential Scripts

---
+++ NewDesign/DEVELOPER/AngularJS_version/public/app/modules/graphs/directives/morris/morrisSalesGraph.js
@@ -0,0 +1,77 @@
+define(['modules/graphs/module', 'morris'], function(module){
+
+    "use strict";
+
+    return module.registerDirective('morrisSalesGraph', function(){
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="chart no-padding"></div>',
+            link: function(scope, element){
+                Morris.Area({
+                    element : element,
+                    data : [{
+                        period : '2010 Q1',
+                        iphone : 2666,
+                        ipad : null,
+                        itouch : 2647
+                    }, {
+                        period : '2010 Q2',
+                        iphone : 2778,
+                        ipad : 2294,
+                        itouch : 2441
+                    }, {
+                        period : '2010 Q3',
+                        iphone : 4912,
+                        ipad : 1969,
+                        itouch : 2501
+                    }, {
+                        period : '2010 Q4',
+                        iphone : 3767,
+                        ipad : 3597,
+                        itouch : 5689
+                    }, {
+                        period : '2011 Q1',
+                        iphone : 6810,
+                        ipad : 1914,
+                        itouch : 2293
+                    }, {
+                        period : '2011 Q2',
+                        iphone : 5670,
+                        ipad : 4293,
+                        itouch : 1881
+                    }, {
+                        period : '2011 Q3',
+                        iphone : 4820,
+                        ipad : 3795,
+                        itouch : 1588
+                    }, {
+                        period : '2011 Q4',
+                        iphone : 15073,
+                        ipad : 5967,
+                        itouch : 5175
+                    }, {
+                        period : '2012 Q1',
+                        iphone : 10687,
+                        ipad : 4460,
+                        itouch : 2028
+                    }, {
+                        period : '2012 Q2',
+                        iphone : 8432,
+                        ipad : 5713,
+                        itouch : 1791
+                    }],
+                    xkey : 'period',
+                    ykeys : ['iphone', 'ipad', 'itouch'],
+                    labels : ['iPhone', 'iPad', 'iPod Touch'],
+                    pointSize : 2,
+                    hideHover : 'auto'
+                });
+
+            }
+        }
+    })
+
+
+
+});
