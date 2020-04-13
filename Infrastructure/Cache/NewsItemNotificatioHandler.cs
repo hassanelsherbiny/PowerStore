89610e7d87d54f624c6c169e3b4421f0f40c955d
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-04-14 00:23:05 +0200

    Implement place order functionality

---
+++ Infrastructure/Cache/NewsItemNotificatioHandler.cs
@@ -0,0 +1,36 @@
+﻿using Grand.Core.Caching;
+using Grand.Domain.News;
+using Grand.Core.Events;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Infrastructure.Cache
+{
+    public class NewsItemNotificatioHandler :
+        INotificationHandler<EntityInserted<NewsItem>>,
+        INotificationHandler<EntityUpdated<NewsItem>>,
+        INotificationHandler<EntityDeleted<NewsItem>>
+    {
+
+        private readonly ICacheBase _cacheBase;
+
+        public NewsItemNotificatioHandler(ICacheBase cacheManager)
+        {
+            _cacheBase = cacheManager;
+        }
+
+        public async Task Handle(EntityInserted<NewsItem> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.NEWS_PATTERN_KEY);
+        }
+        public async Task Handle(EntityUpdated<NewsItem> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.NEWS_PATTERN_KEY);
+        }
+        public async Task Handle(EntityDeleted<NewsItem> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.NEWS_PATTERN_KEY);
+        }
+    }
+}
