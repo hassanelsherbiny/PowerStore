fb6c876492bc6be955944e939a09d0fae293e7f6
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-04-28 02:54:04 +0200

    Implement user profile page

---
+++ Models/Topics/TopicModel.cs
@@ -0,0 +1,34 @@
+﻿using Grand.Core.Models;
+using System;
+
+namespace Grand.Web.Models.Topics
+{
+    public partial class TopicModel : BaseEntityModel
+    {
+        public string SystemName { get; set; }
+
+        public bool IncludeInSitemap { get; set; }
+
+        public bool IsPasswordProtected { get; set; }
+
+        public string Password { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public string MetaKeywords { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public string MetaTitle { get; set; }
+
+        public string SeName { get; set; }
+
+        public string TopicTemplateId { get; set; }
+
+        public bool Published { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
