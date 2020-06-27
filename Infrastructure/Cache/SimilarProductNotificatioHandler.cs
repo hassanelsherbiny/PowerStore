417b9df068f9364abd2810acfb79e7d4fd557794
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2020-06-27 20:51:03 +0200

    Style search results page

---
+++ Infrastructure/Cache/SimilarProductNotificatioHandler.cs
@@ -0,0 +1,39 @@
+﻿using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Core.Events;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Infrastructure.Cache
+{
+    public class SimilarProductNotificatioHandler :
+        INotificationHandler<EntityInserted<SimilarProduct>>,
+        INotificationHandler<EntityUpdated<SimilarProduct>>,
+        INotificationHandler<EntityDeleted<SimilarProduct>>
+    {
+
+        private readonly ICacheBase _cacheBase;
+
+        public SimilarProductNotificatioHandler(ICacheBase cacheManager)
+        {
+            _cacheBase = cacheManager;
+        }
+
+        public async Task Handle(EntityInserted<SimilarProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+        }
+        public async Task Handle(EntityUpdated<SimilarProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+        }
+        public async Task Handle(EntityDeleted<SimilarProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+        }
+    }
+}
