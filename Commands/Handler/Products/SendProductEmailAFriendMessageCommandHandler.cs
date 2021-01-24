b0ba919d6d2cd05399e7c715c28e448931cd41f1
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2021-01-24 14:00:02 +0200

    Added support for multiple shipping options and tracking

---
+++ Commands/Handler/Products/SendProductEmailAFriendMessageCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Grand.Services.Common;
+using Grand.Services.Messages;
+using Grand.Web.Commands.Models.Products;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Commands.Handler.Products
+{
+    public class SendProductEmailAFriendMessageCommandHandler : IRequestHandler<SendProductEmailAFriendMessageCommand, bool>
+    {
+        private readonly IWorkflowMessageService _workflowMessageService;
+
+        public SendProductEmailAFriendMessageCommandHandler(IWorkflowMessageService workflowMessageService)
+        {
+            _workflowMessageService = workflowMessageService;
+        }
+
+        public async Task<bool> Handle(SendProductEmailAFriendMessageCommand request, CancellationToken cancellationToken)
+        {
+            await _workflowMessageService.SendProductEmailAFriendMessage(request.Customer, request.Store,
+                               request.Language.Id, request.Product,
+                               request.Model.YourEmailAddress, request.Model.FriendEmail,
+                               FormatText.ConvertText(request.Model.PersonalMessage));
+
+            return true;
+        }
+    }
+}
