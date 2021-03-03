37c8be78106c8d88f6ed4ab096d2ef1e4d06ebd4
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2021-03-03 23:10:02 +0200

    Enhance UI Component Hover States for Improved User Feedback

---
+++ NewDesign/DEVELOPER/netMVC5_version/webapp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿#region Using
+
+using System.Web.Mvc;
+
+#endregion
+
+namespace SmartAdminMvc.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: home/inbox
+        public ActionResult Inbox()
+        {
+            return View();
+        }
+
+        // GET: home/calendar
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        // GET: home/google-map
+        public ActionResult GoogleMap()
+        {
+            return View();
+        }
+
+        // GET: home/widgets
+        public ActionResult Widgets()
+        {
+            //[TEST] to initialize the theme setter
+            //could be called via jQuery or somewhere...
+            Settings.SetValue<string>("config:CurrentTheme", "smart-style-5");
+
+            return View();
+        }
+
+        // GET: home/chat
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
