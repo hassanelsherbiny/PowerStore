98f431d6baf47a81d3903621d0bf2a477d73bec9
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-04-27 18:58:01 +0200

    Fixed tax calculation issue based on user location

---
+++ Areas/Admin/Extensions/Mapping/Settings/VendorSettingsMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Grand.Core.Mapper;
+using Grand.Domain.Vendors;
+using Grand.Web.Areas.Admin.Models.Settings;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class VendorSettingsMappingExtensions
+    {
+        public static VendorSettingsModel ToModel(this VendorSettings entity)
+        {
+            return entity.MapTo<VendorSettings, VendorSettingsModel>();
+        }
+        public static VendorSettings ToEntity(this VendorSettingsModel model, VendorSettings destination)
+        {
+            return model.MapTo(destination);
+        }
+    }
+}
