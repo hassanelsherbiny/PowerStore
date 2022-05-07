d2a312686fd41803aaa4a4819538069f1ce21d14
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-05-07 16:51:01 +0200

    Implemented order confirmation emails for customers

---
+++ Components/OrderSummary.cs
@@ -0,0 +1,50 @@
+﻿using Grand.Core;
+using Grand.Domain.Orders;
+using Grand.Framework.Components;
+using Grand.Services.Orders;
+using Grand.Web.Features.Models.ShoppingCart;
+using Grand.Web.Models.ShoppingCart;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class OrderSummaryViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IStoreContext _storeContext;
+        private readonly IWorkContext _workContext;
+        public OrderSummaryViewComponent(IMediator mediator, IShoppingCartService shoppingCartService, IStoreContext storeContext, IWorkContext workContext)
+        {
+            _mediator = mediator;
+            _shoppingCartService = shoppingCartService;
+            _storeContext = storeContext;
+            _workContext = workContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(bool? prepareAndDisplayOrderReviewData, ShoppingCartModel overriddenModel)
+        {
+            //use already prepared (shared) model
+            if (overriddenModel != null)
+                return View(overriddenModel);
+
+            var cart = _shoppingCartService.GetShoppingCart(_storeContext.CurrentStore.Id, ShoppingCartType.ShoppingCart, ShoppingCartType.Auctions);
+
+            var model = await _mediator.Send(new GetShoppingCart() {
+                Cart = cart,
+                IsEditable = false,
+                PrepareAndDisplayOrderReviewData = prepareAndDisplayOrderReviewData.GetValueOrDefault(),
+                Customer = _workContext.CurrentCustomer,
+                Currency = _workContext.WorkingCurrency,
+                Language = _workContext.WorkingLanguage,
+                Store = _storeContext.CurrentStore,
+                TaxDisplayType = _workContext.TaxDisplayType
+            });
+
+            return View(model);
+
+        }
+    }
+}
