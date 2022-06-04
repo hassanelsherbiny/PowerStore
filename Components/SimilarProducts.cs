73db84692e5dc0c01aa5c871f4cb99a0ca5e370d
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-06-04 10:40:01 +0200

    Added support for guest checkout to improve user onboarding

---
+++ Components/SimilarProducts.cs
@@ -0,0 +1,69 @@
+﻿using Grand.Core;
+using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Framework.Components;
+using Grand.Services.Catalog;
+using Grand.Web.Features.Models.Products;
+using Grand.Web.Infrastructure.Cache;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class SimilarProductsViewComponent : BaseViewComponent
+    {
+        #region Fields
+        private readonly ICacheBase _cacheBase;
+        private readonly IProductService _productService;
+        private readonly IStoreContext _storeContext;
+        private readonly IMediator _mediator;
+
+        private readonly CatalogSettings _catalogSettings;
+
+        #endregion
+
+        #region Constructors
+        public SimilarProductsViewComponent(
+            ICacheBase cacheManager,
+            IProductService productService,
+            IStoreContext storeContext,
+            IMediator mediator,
+            CatalogSettings catalogSettings)
+        {
+            _cacheBase = cacheManager;
+            _productService = productService;
+            _storeContext = storeContext;
+            _mediator = mediator;
+            _catalogSettings = catalogSettings;
+        }
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId, int? productThumbPictureSize)
+        {
+
+            var productIds = await _cacheBase.GetAsync(string.Format(ModelCacheEventConst.PRODUCTS_SIMILAR_IDS_KEY, productId, _storeContext.CurrentStore.Id),
+                   async () => (await _productService.GetProductById(productId)).SimilarProducts.OrderBy(x => x.DisplayOrder).Select(x => x.ProductId2).ToArray());
+
+            var products = await _productService.GetProductsByIds(productIds);
+
+            if (!products.Any())
+                return Content("");
+
+            var model = await _mediator.Send(new GetProductOverview() {
+                PreparePictureModel = true,
+                PreparePriceModel = true,
+                PrepareSpecificationAttributes = _catalogSettings.ShowSpecAttributeOnCatalogPages,
+                ProductThumbPictureSize = productThumbPictureSize,
+                Products = products
+            });
+
+            return View(model);
+        }
+
+        #endregion
+    }
+}
