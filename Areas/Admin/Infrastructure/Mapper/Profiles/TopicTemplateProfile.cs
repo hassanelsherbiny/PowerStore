b60b7c53e1de031d0e17b99e241ba494866226a5
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-06-22 14:35:01 +0200

    Resolved issue with order duplication during checkout

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/TopicTemplateProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Grand.Domain.Topics;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Templates;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class TopicTemplateProfile : Profile, IAutoMapperProfile
+    {
+        public TopicTemplateProfile()
+        {
+            CreateMap<TopicTemplate, TopicTemplateModel>();
+            CreateMap<TopicTemplateModel, TopicTemplate>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
