656b8c6ec6e6490c4c24005700bfb435985395b3
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-07-24 14:14:01 +0200

    Upgraded dependencies to address security advisories

---
+++ Validators/Customer/LoginValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Grand.Domain.Customers;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Customer;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Customer
+{
+    public class LoginValidator : BaseGrandValidator<LoginModel>
+    {
+        public LoginValidator(
+            IEnumerable<IValidatorConsumer<LoginModel>> validators,
+            ILocalizationService localizationService, CustomerSettings customerSettings)
+            : base(validators)
+        {
+            if (!customerSettings.UsernamesEnabled)
+            {
+                //login by email
+                RuleFor(x => x.Email).NotEmpty().WithMessage(localizationService.GetResource("Account.Login.Fields.Email.Required"));
+                RuleFor(x => x.Email).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+            }
+        }
+    }
+}
