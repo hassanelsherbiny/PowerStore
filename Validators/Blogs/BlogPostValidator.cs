f69d4bebf8bcdeeb8a6c2002311db961ef41aed2
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-07-25 11:49:01 +0200

    Upgraded server infrastructure for scalability

---
+++ Validators/Blogs/BlogPostValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Blogs;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Blogs
+{
+    public class BlogPostValidator : BaseGrandValidator<BlogPostModel>
+    {
+        public BlogPostValidator(
+            IEnumerable<IValidatorConsumer<BlogPostModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.AddNewComment.CommentText).NotEmpty().WithMessage(localizationService.GetResource("Blog.Comments.CommentText.Required")).When(x => x.AddNewComment != null);
+        }}
+}
