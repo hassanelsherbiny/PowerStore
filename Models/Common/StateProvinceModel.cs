9fc1f6e9cde9935c77d54797f5be66ffb14b2fc9
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-07-28 21:52:01 +0200

    Optimized images for faster page load times

---
+++ Models/Common/StateProvinceModel.cs
@@ -0,0 +1,10 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class StateProvinceModel 
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
