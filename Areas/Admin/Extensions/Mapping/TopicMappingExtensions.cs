b898b209dfe0bd6396cf228dda5b7331029ea6d4
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-07-28 03:14:01 +0200

    Updated UI components to match new design guidelines

---
+++ Areas/Admin/Extensions/Mapping/TopicMappingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Grand.Core.Mapper;
+using Grand.Domain.Topics;
+using Grand.Services.Helpers;
+using Grand.Web.Areas.Admin.Models.Topics;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class TopicMappingExtensions
+    {
+        public static TopicModel ToModel(this Topic entity, IDateTimeHelper dateTimeHelper)
+        {
+            var topic = entity.MapTo<Topic, TopicModel>();
+            topic.StartDateUtc = entity.StartDateUtc.ConvertToUserTime(dateTimeHelper);
+            topic.EndDateUtc = entity.EndDateUtc.ConvertToUserTime(dateTimeHelper);
+            return topic;
+        }
+
+        public static Topic ToEntity(this TopicModel model, IDateTimeHelper dateTimeHelper)
+        {
+            var topic = model.MapTo<TopicModel, Topic>();
+            topic.StartDateUtc = model.StartDateUtc.ConvertToUtcTime(dateTimeHelper);
+            topic.EndDateUtc = model.EndDateUtc.ConvertToUtcTime(dateTimeHelper);
+            return topic;
+        }
+
+        public static Topic ToEntity(this TopicModel model, Topic destination, IDateTimeHelper dateTimeHelper)
+        {
+            var topic = model.MapTo(destination);
+            topic.StartDateUtc = model.StartDateUtc.ConvertToUtcTime(dateTimeHelper);
+            topic.EndDateUtc = model.EndDateUtc.ConvertToUtcTime(dateTimeHelper);
+            return topic;
+        }
+    }
+}
