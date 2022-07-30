ce607dfd3ba5abd797bb9022b31cb5d9b9bed282
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-07-31 01:50:01 +0200

    Implemented role-based access control for admin users

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/CustomerTagProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Grand.Domain.Customers;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Customers;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class CustomerTagProfile : Profile, IAutoMapperProfile
+    {
+        public CustomerTagProfile()
+        {
+            CreateMap<CustomerTag, CustomerTagModel>();
+            CreateMap<CustomerTagModel, CustomerTag>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/DocumentTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Grand.Domain.Documents;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Documents;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class DocumentTypeProfile : Profile, IAutoMapperProfile
+    {
+        public DocumentTypeProfile()
+        {
+            CreateMap<DocumentType, DocumentTypeModel>();
+            CreateMap<DocumentTypeModel, DocumentType>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
