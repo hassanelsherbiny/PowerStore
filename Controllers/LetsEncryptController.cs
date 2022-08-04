ee4b749e4c4d5c1506217fbd3a26da8ea61b37ad
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-04 11:47:01 +0200

    Added API rate limiting to prevent abuse

---
+++ Controllers/LetsEncryptController.cs
@@ -0,0 +1,36 @@
+﻿using Grand.Core;
+using Grand.Domain.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Grand.Web.Controllers
+{
+    [AllowAnonymous]
+    public partial class LetsEncryptController : Controller
+    {
+        private readonly CommonSettings _commonSettings;
+
+        public LetsEncryptController(CommonSettings commonSettings)
+        {
+            _commonSettings = commonSettings;
+        }
+        public virtual IActionResult Index(string fileName)
+        {
+            if(!_commonSettings.AllowToReadLetsEncryptFile)
+                return Content("");
+
+            if (fileName == null)
+                return Content("");
+
+            var file = CommonHelper.MapPath("~/wwwroot/content/acme/") + Path.GetFileName(fileName);
+            if (System.IO.File.Exists(file))
+            {
+                return new PhysicalFileResult(file, "text/plain");
+            }
+            return Content("");
+
+        }
+
+    }
+}
---
+++ Models/Common/StoreSelectorModel.cs
@@ -0,0 +1,18 @@
+﻿using Grand.Core.Models;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class StoreSelectorModel : BaseModel
+    {
+        public StoreSelectorModel()
+        {
+            AvailableStores = new List<StoreModel>();
+        }
+
+        public IList<StoreModel> AvailableStores { get; set; }
+
+        public string CurrentStoreId { get; set; }
+
+    }
+}
