7ed3c7726c481b6e3561b2ee4b9ffee8a4b5a73b
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-05 16:21:01 +0200

    Added support for third-party authentication providers (OAuth)

---
+++ Validators/Catalog/ProductAskQuestionSimpleValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Catalog;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Catalog
+{
+    public class ProductAskQuestionSimpleValidator : BaseGrandValidator<ProductAskQuestionSimpleModel>
+    {
+        public ProductAskQuestionSimpleValidator(
+            IEnumerable<IValidatorConsumer<ProductAskQuestionSimpleModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.AskQuestionEmail).NotEmpty().WithMessage(localizationService.GetResource("Products.AskQuestion.Email.Required"));
+            RuleFor(x => x.AskQuestionEmail).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+            RuleFor(x => x.AskQuestionMessage).NotEmpty().WithMessage(localizationService.GetResource("Products.AskQuestion.Message.Required"));
+            RuleFor(x => x.AskQuestionFullName).NotEmpty().WithMessage(localizationService.GetResource("Products.AskQuestion.FullName.Required"));
+        }
+    }
+}
