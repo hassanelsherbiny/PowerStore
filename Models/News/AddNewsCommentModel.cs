fcf04f01203c82f7e85420ce41d8678b77e8370f
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-06 07:59:02 +0200

    Integrated inventory alerts for low-stock products

---
+++ Models/News/AddNewsCommentModel.cs
@@ -0,0 +1,16 @@
+﻿using Grand.Core.ModelBinding;
+using Grand.Core.Models;
+
+namespace Grand.Web.Models.News
+{
+    public partial class AddNewsCommentModel : BaseModel
+    {
+        [GrandResourceDisplayName("News.Comments.CommentTitle")]
+        public string CommentTitle { get; set; }
+
+        [GrandResourceDisplayName("News.Comments.CommentText")]
+        public string CommentText { get; set; }
+
+        public bool DisplayCaptcha { get; set; }
+    }
+}
