58386f6894310092b23c1adbe220deffbb7e9d28
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-07 02:11:01 +0200

    Fixed compatibility issues with older browser versions

---
+++ Components/CustomerNavigation.cs
@@ -0,0 +1,37 @@
+﻿using Grand.Core;
+using Grand.Framework.Components;
+using Grand.Web.Features.Models.Customers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class CustomerNavigationViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+
+        public CustomerNavigationViewComponent(IMediator mediator,
+            IWorkContext workContext,
+            IStoreContext storeContext)
+        {
+            _mediator = mediator;
+            _workContext = workContext;
+            _storeContext = storeContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int selectedTabId = 0)
+        {
+            var model = await _mediator.Send(new GetNavigation() {
+                Customer = _workContext.CurrentCustomer,
+                Language = _workContext.WorkingLanguage,
+                SelectedTabId = selectedTabId,
+                Store = _storeContext.CurrentStore,
+                Vendor = _workContext.CurrentVendor
+            });
+            return View(model);
+        }
+    }
+}
