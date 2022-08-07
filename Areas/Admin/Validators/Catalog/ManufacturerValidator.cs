ba93b58d7251be3fcd0995a4a1ef731d303c9f0e
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-07 20:44:01 +0200

    Updated copyright information for the current year

---
+++ Areas/Admin/Validators/Catalog/ManufacturerValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Areas.Admin.Models.Catalog;
+using System.Collections.Generic;
+
+namespace Grand.Web.Areas.Admin.Validators.Catalog
+{
+    public class ManufacturerValidator : BaseGrandValidator<ManufacturerModel>
+    {
+        public ManufacturerValidator(
+            IEnumerable<IValidatorConsumer<ManufacturerModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Manufacturers.Fields.Name.Required"));
+        }
+    }
+}
