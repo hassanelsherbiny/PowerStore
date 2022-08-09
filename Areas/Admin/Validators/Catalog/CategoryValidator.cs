7ce25ef7d4d17caab6332bbb0765309fc2260f26
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-09 08:30:01 +0200

    Fixed broken links to external resources

---
+++ Areas/Admin/Validators/Catalog/CategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Areas.Admin.Models.Catalog;
+using System.Collections.Generic;
+
+namespace Grand.Web.Areas.Admin.Validators.Catalog
+{
+    public class CategoryValidator : BaseGrandValidator<CategoryModel>
+    {
+        public CategoryValidator(
+            IEnumerable<IValidatorConsumer<CategoryModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Categories.Fields.Name.Required"));
+        }
+    }
+}
---
+++ Features/Handlers/Checkout/GetIsPaymentWorkflowRequiredHandler.cs
@@ -0,0 +1,30 @@
+﻿using Grand.Domain.Payments;
+using Grand.Services.Orders;
+using Grand.Web.Features.Models.Checkout;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Features.Handlers.Checkout
+{
+    public class GetIsPaymentWorkflowRequiredHandler : IRequestHandler<GetIsPaymentWorkflowRequired, bool>
+    {
+        private readonly IOrderTotalCalculationService _orderTotalCalculationService;
+        private readonly PaymentSettings _paymentSettings;
+
+        public GetIsPaymentWorkflowRequiredHandler(IOrderTotalCalculationService orderTotalCalculationService, PaymentSettings paymentSettings)
+        {
+            _orderTotalCalculationService = orderTotalCalculationService;
+            _paymentSettings = paymentSettings;
+        }
+        public async Task<bool> Handle(GetIsPaymentWorkflowRequired request, CancellationToken cancellationToken)
+        {
+            bool result = true;
+            //check whether order total equals zero
+            decimal? shoppingCartTotalBase = (await _orderTotalCalculationService.GetShoppingCartTotal(request.Cart, useRewardPoints: request.UseRewardPoints)).shoppingCartTotal;
+            if (shoppingCartTotalBase.HasValue && shoppingCartTotalBase.Value == decimal.Zero && !_paymentSettings.ShowPaymentMethodIfCartIsZero)
+                result = false;
+            return result;
+        }
+    }
+}
