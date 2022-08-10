3a5bbe9c57e49a938591c64d57a117784fecc236
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-10 14:54:01 +0200

    Fixed SQL injection vulnerability in search function

---
+++ Models/Common/FooterCleanModel.cs
@@ -0,0 +1,14 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class FooterCleanModel : BaseModel
+    {
+        public string StoreName { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyEmail { get; set; }
+        public string CompanyHours { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyPhone { get; set; }
+    }
+}
