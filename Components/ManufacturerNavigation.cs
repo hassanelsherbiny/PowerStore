40c9bc590b5a0ac0b58c2385546c295c527ff4ea
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-11 22:34:01 +0200

    Added unit tests for edge cases and boundary conditions

---
+++ Components/ManufacturerNavigation.cs
@@ -0,0 +1,49 @@
+﻿using Grand.Core;
+using Grand.Domain.Catalog;
+using Grand.Framework.Components;
+using Grand.Web.Features.Models.Catalog;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class ManufacturerNavigationViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly CatalogSettings _catalogSettings;
+
+        public ManufacturerNavigationViewComponent(
+            IMediator mediator,
+            IWorkContext workContext,
+            IStoreContext storeContex,
+            CatalogSettings catalogSettings)
+        {
+            _mediator = mediator;
+            _workContext = workContext;
+            _storeContext = storeContex;
+            _catalogSettings = catalogSettings;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string currentManufacturerId)
+        {
+            if (_catalogSettings.ManufacturersBlockItemsToDisplay == 0)
+                return Content("");
+
+            var model = await _mediator.Send(new GetManufacturerNavigation() {
+                CurrentManufacturerId = currentManufacturerId,
+                Customer = _workContext.CurrentCustomer,
+                Language = _workContext.WorkingLanguage,
+                Store = _storeContext.CurrentStore
+            });
+
+            if (!model.Manufacturers.Any())
+                return Content("");
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/Catalog/TopMenuModel.cs
@@ -0,0 +1,54 @@
+﻿using Grand.Core.Models;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Catalog
+{
+    public partial class TopMenuModel : BaseModel
+    {
+        public TopMenuModel()
+        {
+            Categories = new List<CategorySimpleModel>();
+            Topics = new List<TopMenuTopicModel>();
+            Manufacturers = new List<TopMenuManufacturerModel> ();
+        }
+
+        public IList<CategorySimpleModel> Categories { get; set; }
+        public IList<TopMenuTopicModel> Topics { get; set; }
+        public IList<TopMenuManufacturerModel> Manufacturers { get; set; }
+
+        public bool BlogEnabled { get; set; }
+        public bool NewProductsEnabled { get; set; }
+
+        public bool DisplayHomePageMenu { get; set; }
+        public bool DisplayNewProductsMenu { get; set; }
+        public bool DisplaySearchMenu { get; set; }
+        public bool DisplayCustomerMenu { get; set; }
+        public bool DisplayBlogMenu { get; set; }
+        public bool DisplayContactUsMenu { get; set; }
+
+        #region Nested classes
+
+        public class TopMenuTopicModel : BaseEntityModel
+        {
+            public string Name { get; set; }
+            public string SeName { get; set; }
+        }
+
+        public class TopMenuManufacturerModel : BaseEntityModel
+        {
+            public string Name { get; set; }
+            public string Icon { get; set; }
+            public string SeName { get; set; }
+        }
+
+
+        public class CategoryLineModel : BaseModel
+        {
+            public int Level { get; set; }
+            public bool ResponsiveMobileMenu { get; set; }
+            public CategorySimpleModel Category { get; set; }
+        }
+
+        #endregion
+    }
+}
