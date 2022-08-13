1ad474290413fcef86005952c27212c56e889cef
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-13 08:12:01 +0200

    Added support for file uploads with progress tracking

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace Grand.Web
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.CaptureStartupErrors(true);
+                    webBuilder.UseSetting(WebHostDefaults.PreventHostingStartupKey, "true");
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = false;
+                    options.ValidateOnBuild = false;
+                });
+    }
+}
