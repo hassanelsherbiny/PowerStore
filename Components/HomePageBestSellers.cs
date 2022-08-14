2b0d8f5615f73f84dabd88ebf1d651cba923b6f9
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-14 15:59:01 +0200

    Fixed broken image paths in gallery section

---
+++ Components/HomePageBestSellers.cs
@@ -0,0 +1,83 @@
+﻿using Grand.Core;
+using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Framework.Components;
+using Grand.Services.Catalog;
+using Grand.Services.Orders;
+using Grand.Web.Features.Models.Products;
+using Grand.Web.Infrastructure.Cache;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class HomePageBestSellersViewComponent : BaseViewComponent
+    {
+        #region Fields
+        private readonly IOrderReportService _orderReportService;
+        private readonly ICacheBase _cacheBase;
+        private readonly IStoreContext _storeContext;
+        private readonly IProductService _productService;
+        private readonly IMediator _mediator;
+
+        private readonly CatalogSettings _catalogSettings;
+        #endregion
+
+        #region Constructors
+
+        public HomePageBestSellersViewComponent(
+            IOrderReportService orderReportService,
+            ICacheBase cacheManager,
+            IStoreContext storeContext,
+            IProductService productService,
+            IMediator mediator,
+            CatalogSettings catalogSettings)
+        {
+            _orderReportService = orderReportService;
+            _cacheBase = cacheManager;
+            _storeContext = storeContext;
+            _productService = productService;
+            _mediator = mediator;
+            _catalogSettings = catalogSettings;
+        }
+
+
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(int? productThumbPictureSize)
+        {
+            if (!_catalogSettings.ShowBestsellersOnHomepage || _catalogSettings.NumberOfBestsellersOnHomepage == 0)
+                return Content("");
+
+            //load and cache report
+            var fromdate = DateTime.UtcNow.AddMonths(_catalogSettings.PeriodBestsellers > 0 ? -_catalogSettings.PeriodBestsellers : -12);
+            var report = await _cacheBase.GetAsync(string.Format(ModelCacheEventConst.HOMEPAGE_BESTSELLERS_IDS_KEY, _storeContext.CurrentStore.Id), async () =>
+                                await _orderReportService.BestSellersReport(
+                                    createdFromUtc: fromdate,
+                                    ps: Domain.Payments.PaymentStatus.Paid,
+                                    storeId: _storeContext.CurrentStore.Id,
+                                    pageSize: _catalogSettings.NumberOfBestsellersOnHomepage));
+
+            //load products
+            var products = await _productService.GetProductsByIds(report.Select(x => x.ProductId).ToArray());
+
+            if (!products.Any())
+                return Content("");
+
+            var model = await _mediator.Send(new GetProductOverview() {
+                ProductThumbPictureSize = productThumbPictureSize,
+                Products = products,
+            });
+
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Validators/Customer/ChangePasswordValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Grand.Domain.Customers;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Customer;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Customer
+{
+    public class ChangePasswordValidator : BaseGrandValidator<ChangePasswordModel>
+    {
+        public ChangePasswordValidator(
+            IEnumerable<IValidatorConsumer<ChangePasswordModel>> validators,
+            ILocalizationService localizationService, CustomerSettings customerSettings)
+            : base(validators)
+        {
+            RuleFor(x => x.OldPassword).NotEmpty().WithMessage(localizationService.GetResource("Account.ChangePassword.Fields.OldPassword.Required"));
+            RuleFor(x => x.NewPassword).NotEmpty().WithMessage(localizationService.GetResource("Account.ChangePassword.Fields.NewPassword.Required"));
+            RuleFor(x => x.NewPassword).Length(customerSettings.PasswordMinLength, 999).WithMessage(string.Format(localizationService.GetResource("Account.ChangePassword.Fields.NewPassword.LengthValidation"), customerSettings.PasswordMinLength));
+
+            if (!string.IsNullOrEmpty(customerSettings.PasswordRegularExpression))
+                RuleFor(x => x.NewPassword).Matches(customerSettings.PasswordRegularExpression).WithMessage(string.Format(localizationService.GetResource("Account.ChangePassword.Fields.NewPassword.Validation")));
+
+            RuleFor(x => x.ConfirmNewPassword).NotEmpty().WithMessage(localizationService.GetResource("Account.ChangePassword.Fields.ConfirmNewPassword.Required"));
+            RuleFor(x => x.ConfirmNewPassword).Equal(x => x.NewPassword).WithMessage(localizationService.GetResource("Account.ChangePassword.Fields.NewPassword.EnteredPasswordsDoNotMatch"));
+        }
+    }
+}
