abbed635172b90a22a7e3dd03bbb0fd4a449d475
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-16 05:16:02 +0200

    Fixed pagination issues in product listings

---
+++ Validators/Knowledgebase/KnowledgebaseArticleValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Knowledgebase;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Knowledgebase
+{
+    public class KnowledgebaseArticleValidator : BaseGrandValidator<KnowledgebaseArticleModel>
+    {
+        public KnowledgebaseArticleValidator(
+            IEnumerable<IValidatorConsumer<KnowledgebaseArticleModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.AddNewComment.CommentText).NotEmpty().WithMessage(localizationService.GetResource("Grand.knowledgebase.addarticlecomment.result")).When(x => x.AddNewComment != null);
+        }
+    }
+}
