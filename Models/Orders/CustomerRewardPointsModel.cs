934fa6b5e1fa5f47144112cace6210a50959d5a1
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-22 06:29:06 +0200

    Added data encryption to protect sensitive information

---
+++ Models/Orders/CustomerRewardPointsModel.cs
@@ -0,0 +1,40 @@
+﻿using Grand.Core.ModelBinding;
+using Grand.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Orders
+{
+    public partial class CustomerRewardPointsModel : BaseModel
+    {
+        public CustomerRewardPointsModel()
+        {
+            RewardPoints = new List<RewardPointsHistoryModel>();
+        }
+
+        public IList<RewardPointsHistoryModel> RewardPoints { get; set; }
+        public int RewardPointsBalance { get; set; }
+        public string RewardPointsAmount { get; set; }
+        public int MinimumRewardPointsBalance { get; set; }
+        public string MinimumRewardPointsAmount { get; set; }
+
+        #region Nested classes
+
+        public partial class RewardPointsHistoryModel : BaseEntityModel
+        {
+            [GrandResourceDisplayName("RewardPoints.Fields.Points")]
+            public int Points { get; set; }
+
+            [GrandResourceDisplayName("RewardPoints.Fields.PointsBalance")]
+            public int PointsBalance { get; set; }
+
+            [GrandResourceDisplayName("RewardPoints.Fields.Message")]
+            public string Message { get; set; }
+
+            [GrandResourceDisplayName("RewardPoints.Fields.Date")]
+            public DateTime CreatedOn { get; set; }
+        }
+
+        #endregion
+    }
+}
---
+++ Validators/ShoppingCart/WishlistEmailAFriendValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.ShoppingCart;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.ShoppingCart
+{
+    public class WishlistEmailAFriendValidator : BaseGrandValidator<WishlistEmailAFriendModel>
+    {
+        public WishlistEmailAFriendValidator(
+            IEnumerable<IValidatorConsumer<WishlistEmailAFriendModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.FriendEmail).NotEmpty().WithMessage(localizationService.GetResource("Wishlist.EmailAFriend.FriendEmail.Required"));
+            RuleFor(x => x.FriendEmail).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+
+            RuleFor(x => x.YourEmailAddress).NotEmpty().WithMessage(localizationService.GetResource("Wishlist.EmailAFriend.YourEmailAddress.Required"));
+            RuleFor(x => x.YourEmailAddress).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+        }}
+}
