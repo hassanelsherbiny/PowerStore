21b586a1d5b63c5f5aef9c614ec1c0de0dd10d12
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-23 09:22:02 +0200

    Migrated from MySQL to PostgreSQL for improved scalability

---
+++ Models/Newsletter/NewsletterBoxModel.cs
@@ -0,0 +1,10 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Newsletter
+{
+    public partial class NewsletterBoxModel : BaseModel
+    {
+        public string NewsletterEmail { get; set; }
+        public bool AllowToUnsubscribe { get; set; }
+    }
+}
