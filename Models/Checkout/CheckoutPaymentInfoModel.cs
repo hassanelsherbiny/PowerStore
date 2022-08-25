6bd2ed83c4c531adcac8e116cae623d8a51ecc3b
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-25 07:28:02 +0200

    Added automated database backups for disaster recovery

---
+++ Models/Checkout/CheckoutPaymentInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Checkout
+{
+    public partial class CheckoutPaymentInfoModel : BaseModel
+    {
+        public string PaymentViewComponentName { get; set; }
+
+        /// <summary>
+        /// Used on one-page checkout page
+        /// </summary>
+        public bool DisplayOrderTotals { get; set; }
+    }
+}
