9ab7898f3feb3141773d8ae18857f8af5c1f9c7e
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-25 19:09:02 +0200

    Optimized network requests to reduce latency

---
+++ Commands/Handler/Products/SendProductAskQuestionMessageCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Grand.Services.Common;
+using Grand.Services.Messages;
+using Grand.Web.Commands.Models.Products;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Commands.Handler.Products
+{
+    public class SendProductAskQuestionMessageCommandHandler : IRequestHandler<SendProductAskQuestionMessageCommand, bool>
+    {
+        private readonly IWorkflowMessageService _workflowMessageService;
+
+        public SendProductAskQuestionMessageCommandHandler(IWorkflowMessageService workflowMessageService)
+        {
+            _workflowMessageService = workflowMessageService;
+        }
+
+        public async Task<bool> Handle(SendProductAskQuestionMessageCommand request, CancellationToken cancellationToken)
+        {
+            await _workflowMessageService.SendProductQuestionMessage(request.Customer, request.Store,
+                               request.Language.Id, request.Product, request.Model.Email, request.Model.FullName, request.Model.Phone,
+                               FormatText.ConvertText(request.Model.Message));
+
+            return true;
+        }
+    }
+}
