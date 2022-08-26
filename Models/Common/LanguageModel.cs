6c74a6d41aac060eb872ecb81ab69e0d8939884a
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-26 13:00:01 +0200

    Reorganized documentation for better user guidance

---
+++ Models/Common/LanguageModel.cs
@@ -0,0 +1,12 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class LanguageModel : BaseEntityModel
+    {
+        public string Name { get; set; }
+
+        public string FlagImageFileName { get; set; }
+
+    }
+}
---
+++ Validators/Catalog/ProductReviewsValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Catalog;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Catalog
+{
+    public class ProductReviewsValidator : BaseGrandValidator<AddProductReviewModel>
+    {
+        public ProductReviewsValidator(
+            IEnumerable<IValidatorConsumer<AddProductReviewModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage(localizationService.GetResource("Reviews.Fields.Title.Required"));
+            RuleFor(x => x.Title).Length(1, 200).WithMessage(string.Format(localizationService.GetResource("Reviews.Fields.Title.MaxLengthValidation"), 200));
+            RuleFor(x => x.ReviewText).NotEmpty().WithMessage(localizationService.GetResource("Reviews.Fields.ReviewText.Required"));
+        }
+    }
+}
