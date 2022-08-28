cef9f10ac20069ca16731d132ea385fc34b3e384
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-28 18:34:02 +0200

    Resolved performance bottleneck in search feature

---
+++ Models/Common/TaxTypeSelectorModel.cs
@@ -0,0 +1,10 @@
+﻿using Grand.Domain.Tax;
+using Grand.Core.Models;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class TaxTypeSelectorModel : BaseModel
+    {
+        public TaxDisplayType CurrentTaxType { get; set; }
+    }
+}
