e67067a63e849127aa13f01f6b0c2b0d3ba8591a
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-28 08:43:02 +0200

    Refactored data models for improved data integrity

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/StateProvinceProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Grand.Domain.Directory;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Extensions;
+using Grand.Web.Areas.Admin.Models.Directory;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class StateProvinceProfile : Profile, IAutoMapperProfile
+    {
+        public StateProvinceProfile()
+        {
+            CreateMap<StateProvince, StateProvinceModel>()
+                .ForMember(dest => dest.Locales, mo => mo.Ignore());
+            CreateMap<StateProvinceModel, StateProvince>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore())
+                .ForMember(dest => dest.Locales, mo => mo.MapFrom(x => x.Locales.ToLocalizedProperty()));
+        }
+
+        public int Order => 0;
+    }
+}
---
+++ Components/ProductsAlsoPurchased.cs
@@ -0,0 +1,82 @@
+﻿using Grand.Core;
+using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Framework.Components;
+using Grand.Services.Catalog;
+using Grand.Services.Orders;
+using Grand.Web.Features.Models.Products;
+using Grand.Web.Infrastructure.Cache;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class ProductsAlsoPurchasedViewComponent : BaseViewComponent
+    {
+        #region Fields
+        private readonly IProductService _productService;
+        private readonly IMediator _mediator;
+        private readonly ICacheBase _cacheBase;
+        private readonly IOrderReportService _orderReportService;
+        private readonly IStoreContext _storeContext;
+        private readonly CatalogSettings _catalogSettings;
+        #endregion
+
+        #region Constructors
+
+        public ProductsAlsoPurchasedViewComponent(
+            IProductService productService,
+            IMediator mediator,
+            ICacheBase cacheManager,
+            IOrderReportService orderReportService,
+            IStoreContext storeContext,
+            CatalogSettings catalogSettings
+)
+        {
+            _productService = productService;
+            _catalogSettings = catalogSettings;
+            _mediator = mediator;
+            _cacheBase = cacheManager;
+            _orderReportService = orderReportService;
+            _storeContext = storeContext;
+        }
+
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(string productId, int? productThumbPictureSize)
+        {
+            if (!_catalogSettings.ProductsAlsoPurchasedEnabled)
+                return Content("");
+
+            //load and cache report
+            var productIds = await _cacheBase.GetAsync(string.Format(ModelCacheEventConst.PRODUCTS_ALSO_PURCHASED_IDS_KEY, productId, _storeContext.CurrentStore.Id),
+                () =>
+                    _orderReportService
+                    .GetAlsoPurchasedProductsIds(_storeContext.CurrentStore.Id, productId, _catalogSettings.ProductsAlsoPurchasedNumber)
+                    );
+
+            //load products
+            var products = await _productService.GetProductsByIds(productIds);
+
+            if (!products.Any())
+                return Content("");
+
+            //prepare model
+            var model = await _mediator.Send(new GetProductOverview() {
+                PreparePictureModel = true,
+                PreparePriceModel = true,
+                ProductThumbPictureSize = productThumbPictureSize,
+                Products = products,
+            });
+
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
