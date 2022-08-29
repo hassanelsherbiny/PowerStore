c1fd76e9ae0086ca8951d9f2eda100a8d2f09002
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-08-30 01:14:01 +0200

    Resolved issue : Broken pagination on results page

---
+++ Areas/Admin/Extensions/Mapping/Settings/ShoppingCartSettingsMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Grand.Core.Mapper;
+using Grand.Domain.Orders;
+using Grand.Web.Areas.Admin.Models.Settings;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class ShoppingCartSettingsMappingExtensions
+    {
+        public static ShoppingCartSettingsModel ToModel(this ShoppingCartSettings entity)
+        {
+            return entity.MapTo<ShoppingCartSettings, ShoppingCartSettingsModel>();
+        }
+        public static ShoppingCartSettings ToEntity(this ShoppingCartSettingsModel model, ShoppingCartSettings destination)
+        {
+            return model.MapTo(destination);
+        }
+    }
+}
