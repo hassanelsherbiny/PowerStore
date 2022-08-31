b6f5350237d72177472fe59aa6fc87be9e0bb1ca
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-09-01 01:58:01 +0200

    Added support for real-time notifications using WebSockets

---
+++ Components/NewsRssHeaderLink.cs
@@ -0,0 +1,24 @@
+﻿using Grand.Domain.News;
+using Grand.Framework.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.ViewComponents
+{
+    public class NewsRssHeaderLinkViewComponent : BaseViewComponent
+    {
+        private readonly NewsSettings _newsSettings;
+        public NewsRssHeaderLinkViewComponent(NewsSettings newsSettings)
+        {
+            _newsSettings = newsSettings;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (!_newsSettings.Enabled || !_newsSettings.ShowHeaderRssUrl)
+                return Content("");
+
+            return View();
+
+        }
+    }
+}
---
+++ Models/Orders/CustomerReturnRequestsModel.cs
@@ -0,0 +1,27 @@
+﻿using Grand.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Orders
+{
+    public partial class CustomerReturnRequestsModel : BaseModel
+    {
+        public CustomerReturnRequestsModel()
+        {
+            Items = new List<ReturnRequestModel>();
+        }
+
+        public IList<ReturnRequestModel> Items { get; set; }
+
+        #region Nested classes
+        public partial class ReturnRequestModel : BaseEntityModel
+        {
+            public int ReturnNumber { get; set; }
+            public string ReturnRequestStatus { get; set; }
+            public DateTime CreatedOn { get; set; }
+            public int ProductsCount { get; set; }
+            public string ReturnTotal { get; set; }
+        }
+        #endregion
+    }
+}
