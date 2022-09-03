d98b83baa2fd7c82252e04a5758748b2d4981f19
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-09-03 07:17:01 +0200

    Implemented two-factor authentication for enhanced security

---
+++ Validators/News/NewsItemValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.News;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.News
+{
+    public class NewsItemValidator : BaseGrandValidator<NewsItemModel>
+    {
+        public NewsItemValidator(
+            IEnumerable<IValidatorConsumer<NewsItemModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.AddNewComment.CommentTitle).NotEmpty().WithMessage(localizationService.GetResource("News.Comments.CommentTitle.Required")).When(x => x.AddNewComment != null);
+            RuleFor(x => x.AddNewComment.CommentTitle).Length(1, 200).WithMessage(string.Format(localizationService.GetResource("News.Comments.CommentTitle.MaxLengthValidation"), 200)).When(x => x.AddNewComment != null && !string.IsNullOrEmpty(x.AddNewComment.CommentTitle));
+            RuleFor(x => x.AddNewComment.CommentText).NotEmpty().WithMessage(localizationService.GetResource("News.Comments.CommentText.Required")).When(x => x.AddNewComment != null);
+        }}
+}
