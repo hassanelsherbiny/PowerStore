107404a75e4e7134fbf0cc45c7cffc5c9810c774
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-09-10 12:34:02 +0200

    Implemented abandoned cart recovery emails

---
+++ Components/NewsletterBox.cs
@@ -0,0 +1,27 @@
+﻿using Grand.Framework.Components;
+using Grand.Web.Features.Models.Newsletter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class NewsletterBoxViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+
+        public NewsletterBoxViewComponent(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _mediator.Send(new GetNewsletterBox());
+            if (model == null)
+                return Content("");
+
+            return View(model);
+        }
+    }
+}
