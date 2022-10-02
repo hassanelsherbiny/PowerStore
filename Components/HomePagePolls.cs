7feb948e4ae9f487ddb5f5cf3e96e57bbcac22f4
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-10-02 16:38:01 +0200

    Added shopping cart functionality for seamless user experience

---
+++ Components/HomePagePolls.cs
@@ -0,0 +1,28 @@
+﻿using Grand.Framework.Components;
+using Grand.Web.Features.Models.Polls;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class HomePagePollsViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+
+        public HomePagePollsViewComponent(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _mediator.Send(new GetHomePagePolls());
+            if (!model.Any())
+                Content("");
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/Blogs/AddBlogCommentModel.cs
@@ -0,0 +1,13 @@
+﻿using Grand.Core.ModelBinding;
+using Grand.Core.Models;
+
+namespace Grand.Web.Models.Blogs
+{
+    public partial class AddBlogCommentModel : BaseEntityModel
+    {
+        [GrandResourceDisplayName("Blog.Comments.CommentText")]
+        public string CommentText { get; set; }
+
+        public bool DisplayCaptcha { get; set; }
+    }
+}
