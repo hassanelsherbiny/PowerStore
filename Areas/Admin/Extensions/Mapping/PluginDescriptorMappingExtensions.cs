ffa81643cf83a511d08cb81dd6e28eb5ef34c543
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-10-09 01:12:01 +0200

    Implemented user authentication and account management

---
+++ Areas/Admin/Extensions/Mapping/PluginDescriptorMappingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Grand.Core.Mapper;
+using Grand.Core.Plugins;
+using Grand.Web.Areas.Admin.Models.Plugins;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class PluginDescriptorMappingExtensions
+    {
+        public static PluginModel ToModel(this PluginDescriptor entity)
+        {
+            return entity.MapTo<PluginDescriptor, PluginModel>();
+        }
+    }
+}
