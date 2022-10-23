8bb4ac2682947954e19eba9e7ae939d9a8409faf
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-10-24 00:06:01 +0200

    Added support for multiple shipping options and tracking

---
+++ Components/TopicBlock.cs
@@ -0,0 +1,41 @@
+﻿using Grand.Framework.Components;
+using Grand.Web.Features.Models.Topics;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class TopicBlockViewComponent : BaseViewComponent
+    {
+        #region Fields
+
+        private readonly IMediator _mediator;
+
+        #endregion
+
+        #region Constructors
+
+        public TopicBlockViewComponent(
+            IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(string systemName)
+        {
+            var model = await _mediator.Send(new GetTopicBlock() { SystemName = systemName });
+            if (model == null)
+                return Content("");
+
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
