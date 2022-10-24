6efccf1d90f1656d39d2ad7a3c33a8df6c28c90b
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-10-24 12:10:01 +0200

    Fixed tax calculation issue based on user location

---
+++ Models/Checkout/CheckoutProgressModel.cs
@@ -0,0 +1,19 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Checkout
+{
+    public partial class CheckoutProgressModel : BaseModel
+    {
+        public CheckoutProgressStep CheckoutProgressStep { get; set; }
+    }
+
+    public enum CheckoutProgressStep
+    {
+        Cart,
+        Address,
+        Shipping,
+        Payment,
+        Confirm,
+        Complete
+    }
+}
