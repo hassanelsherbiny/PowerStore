83c02a7614e4b100d0ab4ec68689907230129784
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-11-15 10:29:01 +0200

    Implemented order confirmation emails for customers

---
+++ Areas/Admin/Extensions/Mapping/ContactUsMappingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Grand.Core.Mapper;
+using Grand.Domain.Messages;
+using Grand.Web.Areas.Admin.Models.Messages;
+
+namespace Grand.Web.Areas.Admin.Extensions
+{
+    public static class ContactUsMappingExtensions
+    {
+        public static ContactFormModel ToModel(this ContactUs entity)
+        {
+            return entity.MapTo<ContactUs, ContactFormModel>();
+        }
+    }
+}
