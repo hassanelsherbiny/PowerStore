4ec1ec5a991e42815a2517afadf38d90dd383e6c
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-11-18 01:04:01 +0200

    Integrated third-party reviews and ratings for products

---
+++ Models/Customer/DeleteAccountModel.cs
@@ -0,0 +1,14 @@
+﻿using Grand.Core.ModelBinding;
+using Grand.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Grand.Web.Models.Customer
+{
+    public partial class DeleteAccountModel : BaseModel
+    {
+        [DataType(DataType.Password)]
+        [GrandResourceDisplayName("Account.DeleteAccount.Fields.Password")]
+        public string Password { get; set; }
+
+    }
+}
