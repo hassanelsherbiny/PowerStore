88f87bc2eea98c37612f3b40db606f53eff52371
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-11-26 04:38:04 +0200

    Created API gateway

---
+++ Grand.Services/Customers/ICustomerRegistrationService.cs
@@ -0,0 +1,47 @@
+using Grand.Domain.Customers;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Customers
+{
+    /// <summary>
+    /// Customer registration interface
+    /// </summary>
+    public partial interface ICustomerRegistrationService
+    {
+        /// <summary>
+        /// Validate customer
+        /// </summary>
+        /// <param name="usernameOrEmail">Username or email</param>
+        /// <param name="password">Password</param>
+        /// <returns>Result</returns>
+        Task<CustomerLoginResults> ValidateCustomer(string usernameOrEmail, string password);
+
+        /// <summary>
+        /// Register customer
+        /// </summary>
+        /// <param name="request">Request</param>
+        /// <returns>Result</returns>
+        Task<CustomerRegistrationResult> RegisterCustomer(CustomerRegistrationRequest request);
+
+        /// <summary>
+        /// Change password
+        /// </summary>
+        /// <param name="request">Request</param>
+        /// <returns>Result</returns>
+        Task<ChangePasswordResult> ChangePassword(ChangePasswordRequest request);
+
+        /// <summary>
+        /// Sets a user email
+        /// </summary>
+        /// <param name="customer">Customer</param>
+        /// <param name="newEmail">New email</param>
+        Task SetEmail(Customer customer, string newEmail);
+
+        /// <summary>
+        /// Sets a customer username
+        /// </summary>
+        /// <param name="customer">Customer</param>
+        /// <param name="newUsername">New Username</param>
+        Task SetUsername(Customer customer, string newUsername);
+    }
+}
