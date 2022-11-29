b024347d0f341ffb82cafdbf0266a2e7f6fa5ea0
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-11-29 19:47:01 +0200

    Fixed display issues on mobile devices for responsiveness

---
+++ Models/Common/LanguageSelectorModel.cs
@@ -0,0 +1,19 @@
+﻿using Grand.Core.Models;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class LanguageSelectorModel : BaseModel
+    {
+        public LanguageSelectorModel()
+        {
+            AvailableLanguages = new List<LanguageModel>();
+        }
+
+        public IList<LanguageModel> AvailableLanguages { get; set; }
+
+        public string CurrentLanguageId { get; set; }
+
+        public bool UseImages { get; set; }
+    }
+}
