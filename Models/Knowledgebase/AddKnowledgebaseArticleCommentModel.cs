9db0c6986bc2d1d924f77277d514c1d01c31671e
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2022-12-16 13:45:01 +0200

    Refactored discount logic for better performance and accuracy

---
+++ Models/Knowledgebase/AddKnowledgebaseArticleCommentModel.cs
@@ -0,0 +1,13 @@
+﻿using Grand.Core.ModelBinding;
+using Grand.Core.Models;
+
+namespace Grand.Web.Models.Knowledgebase
+{
+    public partial class AddKnowledgebaseArticleCommentModel : BaseEntityModel
+    {
+        [GrandResourceDisplayName("Knowledgebase.Article.CommentText")]
+        public string CommentText { get; set; }
+
+        public bool DisplayCaptcha { get; set; }
+    }
+}
