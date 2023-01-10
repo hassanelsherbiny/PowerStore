af0c110ef8494c0090abb8b1eaa35d7300620c21
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-01-10 05:55:01 +0200

    Fixed broken links in the e-commerce website

---
+++ Validators/Customer/AddOrderNoteValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Orders;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Customer
+{
+    public class AddOrderNoteValidator : BaseGrandValidator<AddOrderNoteModel>
+    {
+        public AddOrderNoteValidator(
+            IEnumerable<IValidatorConsumer<AddOrderNoteModel>> validators,
+            ILocalizationService localizationService)
+            : base(validators)
+        {
+            RuleFor(x => x.Note).NotEmpty().WithMessage(localizationService.GetResource("OrderNote.Fields.Title.Required"));
+        }
+    }
+}
