ef08804a0e6d9aac54a0afab3d13c92c5de30411
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-04-19 21:29:01 +0200

    Add basic HTML and CSS files

---
+++ Infrastructure/Cache/ProductCategoryNotificatioHandler.cs
@@ -0,0 +1,39 @@
+﻿using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Core.Events;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Infrastructure.Cache
+{
+    public class ProductCategoryNotificatioHandler :
+        INotificationHandler<EntityInserted<ProductCategory>>,
+        INotificationHandler<EntityUpdated<ProductCategory>>,
+        INotificationHandler<EntityDeleted<ProductCategory>>
+    {
+
+        private readonly ICacheBase _cacheBase;
+
+        public ProductCategoryNotificatioHandler(ICacheBase cacheManager)
+        {
+            _cacheBase = cacheManager;
+        }
+
+        public async Task Handle(EntityInserted<ProductCategory> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.PRODUCT_BREADCRUMB_PATTERN_KEY);
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.CATEGORY_HAS_FEATURED_PRODUCTS_MODEL_KEY, eventMessage.Entity.CategoryId));
+        }
+        public async Task Handle(EntityUpdated<ProductCategory> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.PRODUCT_BREADCRUMB_PATTERN_KEY);
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.CATEGORY_HAS_FEATURED_PRODUCTS_MODEL_KEY, eventMessage.Entity.CategoryId));
+        }
+        public async Task Handle(EntityDeleted<ProductCategory> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.PRODUCT_BREADCRUMB_PATTERN_KEY);
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.CATEGORY_HAS_FEATURED_PRODUCTS_MODEL_KEY, eventMessage.Entity.CategoryId));
+        }
+    }
+}
