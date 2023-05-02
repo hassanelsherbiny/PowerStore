039835334429d7e440bfd7d05a14750d84fabcd7
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-05-02 23:06:02 +0300

    Implement product listing functionality

---
+++ Models/Catalog/CustomerBackInStockSubscriptionsModel.cs
@@ -0,0 +1,29 @@
+﻿using Grand.Core.Models;
+using Grand.Web.Models.Common;
+using System.Collections.Generic;
+
+namespace Grand.Web.Models.Catalog
+{
+    public partial class CustomerBackInStockSubscriptionsModel
+    {
+        public CustomerBackInStockSubscriptionsModel()
+        {
+            Subscriptions = new List<BackInStockSubscriptionModel>();
+        }
+
+        public IList<BackInStockSubscriptionModel> Subscriptions { get; set; }
+        public PagerModel PagerModel { get; set; }
+
+        #region Nested classes
+
+        public partial class BackInStockSubscriptionModel : BaseEntityModel
+        {
+            public string ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string AttributeDescription { get; set; }
+            public string SeName { get; set; }
+        }
+
+        #endregion
+    }
+}
