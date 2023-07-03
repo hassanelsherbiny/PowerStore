4a9a8797cd48f52961cb3347df58a4382cf13e55
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-07-03 20:52:01 +0300

    Update cart icon dynamically

---
+++ Infrastructure/Cache/VendorNotificatioHandler.cs
@@ -0,0 +1,36 @@
+﻿using Grand.Core.Caching;
+using Grand.Domain.Vendors;
+using Grand.Core.Events;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Infrastructure.Cache
+{
+    public class VendorNotificatioHandler :
+        INotificationHandler<EntityInserted<Vendor>>,
+        INotificationHandler<EntityUpdated<Vendor>>,
+        INotificationHandler<EntityDeleted<Vendor>>
+    {
+
+        private readonly ICacheBase _cacheBase;
+
+        public VendorNotificatioHandler(ICacheBase cacheManager)
+        {
+            _cacheBase = cacheManager;
+        }
+
+        public async Task Handle(EntityInserted<Vendor> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.VENDOR_NAVIGATION_PATTERN_KEY);
+        }
+        public async Task Handle(EntityUpdated<Vendor> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.VENDOR_NAVIGATION_PATTERN_KEY);
+        }
+        public async Task Handle(EntityDeleted<Vendor> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(ModelCacheEventConst.VENDOR_NAVIGATION_PATTERN_KEY);
+        }
+    }
+}
