fccdeddc01b2fdf55903544afd36dcad1c936c44
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-07-11 03:18:03 +0300

    Added split testing functionality

---
+++ Grand.Core/Data/MongoDBDataProvider.cs
@@ -0,0 +1,23 @@
+﻿
+using Grand.Domain.Data;
+
+namespace Grand.Core.Data
+{
+    public class MongoDBDataProvider : IDataProvider
+    {
+        #region Methods
+
+        /// <summary>
+        /// Initialize database
+        /// </summary>
+        public virtual void InitDatabase()
+        {
+            DataSettingsHelper.InitConnectionString();
+        }
+
+        #endregion
+
+        public string ConnectionString => DataSettingsHelper.ConnectionString();
+
+    }
+}
