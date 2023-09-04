5f09d20388799edbc9ca323fe8334b3009c73aa6
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-09-04 12:24:01 +0300

    Implement address management

---
+++ Components/CheckoutProgress.cs
@@ -0,0 +1,16 @@
+﻿using Grand.Framework.Components;
+using Grand.Web.Models.Checkout;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.ViewComponents
+{
+    public class CheckoutProgressViewComponent : BaseViewComponent
+    {
+        public IViewComponentResult Invoke(CheckoutProgressStep step)
+        {
+            var model = new CheckoutProgressModel { CheckoutProgressStep = step };
+            return View(model);
+
+        }
+    }
+}
