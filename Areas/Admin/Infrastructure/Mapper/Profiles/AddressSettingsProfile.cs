8c85bbc753093ae55e3410dd7b6db8c7c873cdb3
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-10-10 21:28:01 +0300

    Style category filtering UI

---
+++ Areas/Admin/Infrastructure/Mapper/Profiles/AddressSettingsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Grand.Domain.Common;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Settings;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class AddressSettingsProfile : Profile, IAutoMapperProfile
+    {
+        public AddressSettingsProfile()
+        {
+            CreateMap<AddressSettings, CustomerUserSettingsModel.AddressSettingsModel>()
+                .ForMember(dest => dest.GenericAttributes, mo => mo.Ignore());
+            CreateMap<CustomerUserSettingsModel.AddressSettingsModel, AddressSettings>();
+        }
+
+        public int Order => 0;
+    }
+}
