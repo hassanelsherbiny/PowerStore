556b0927b428384cbc303815e1b2127bcfe37b9d
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-10-24 03:18:04 +0300

    Improved social media engagement

---
+++ Grand.Core/DependencyInjection/IDependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Grand.Core.Configuration;
+using Grand.Core.TypeFinders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grand.Core.DependencyInjection
+{
+    /// <summary>
+    /// Dependency registrar interface
+    /// </summary>
+    public interface IDependencyInjection
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="serviceCollection">Service Collection</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="config">Config</param>
+        void Register(IServiceCollection serviceCollection, ITypeFinder typeFinder, GrandConfig config);
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        int Order { get; }
+    }
+}
