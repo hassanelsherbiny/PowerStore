0396c668d9984197e7eabfb8c0662972cee788c9
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-10-30 17:10:01 +0200

    Style product reviews UI

---
+++ Components/HomePageNewProducts.cs
@@ -0,0 +1,73 @@
+﻿using Grand.Core;
+using Grand.Domain.Catalog;
+using Grand.Framework.Components;
+using Grand.Services.Queries.Models.Catalog;
+using Grand.Web.Features.Models.Products;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Components
+{
+    public class HomePageNewProductsViewComponent : BaseViewComponent
+    {
+        #region Fields
+
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly IMediator _mediator;
+        private readonly CatalogSettings _catalogSettings;
+
+        #endregion
+
+        #region Constructors
+
+        public HomePageNewProductsViewComponent(
+            IWorkContext workContext,
+            IStoreContext storeContext,
+            IMediator mediator,
+            CatalogSettings catalogSettings)
+        {
+            _workContext = workContext;
+            _storeContext = storeContext;
+            _mediator = mediator;
+            _catalogSettings = catalogSettings;
+        }
+
+        #endregion
+
+        #region Invoker
+
+        public async Task<IViewComponentResult> InvokeAsync(int? productThumbPictureSize)
+        {
+            if (!_catalogSettings.NewProductsOnHomePage)
+                return Content("");
+
+            var products = (await _mediator.Send(new GetSearchProductsQuery() {
+                Customer = _workContext.CurrentCustomer,
+                StoreId = _storeContext.CurrentStore.Id,
+                VisibleIndividuallyOnly = true,
+                MarkedAsNewOnly = true,
+                OrderBy = ProductSortingEnum.CreatedOn,
+                PageSize = _catalogSettings.NewProductsNumberOnHomePage
+            })).products;
+
+            if (!products.Any())
+                return Content("");
+
+            var model = await _mediator.Send(new GetProductOverview() {
+                PreparePictureModel = true,
+                PreparePriceModel = true,
+                PrepareSpecificationAttributes = _catalogSettings.ShowSpecAttributeOnCatalogPages,
+                ProductThumbPictureSize = productThumbPictureSize,
+                Products = products,
+            });
+
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Validators/Common/ContactUsValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Grand.Domain.Common;
+using Grand.Core.Validators;
+using Grand.Services.Localization;
+using Grand.Web.Models.Common;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Common
+{
+    public class ContactUsValidator : BaseGrandValidator<ContactUsModel>
+    {
+        public ContactUsValidator(
+            IEnumerable<IValidatorConsumer<ContactUsModel>> validators,
+            ILocalizationService localizationService, CommonSettings commonSettings)
+            : base(validators)
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage(localizationService.GetResource("ContactUs.Email.Required"));
+            RuleFor(x => x.Email).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+            RuleFor(x => x.FullName).NotEmpty().WithMessage(localizationService.GetResource("ContactUs.FullName.Required"));
+            if (commonSettings.SubjectFieldOnContactUsForm)
+            {
+                RuleFor(x => x.Subject).NotEmpty().WithMessage(localizationService.GetResource("ContactUs.Subject.Required"));
+            }
+            RuleFor(x => x.Enquiry).NotEmpty().WithMessage(localizationService.GetResource("ContactUs.Enquiry.Required"));
+        }}
+}
