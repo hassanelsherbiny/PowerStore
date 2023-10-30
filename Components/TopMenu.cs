cd0af2c2549b5d73475573964a65ce36b4cca93d
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-10-30 05:56:01 +0200

    Implement product reviews functionality

---
+++ Components/TopMenu.cs
@@ -0,0 +1,36 @@
+﻿using Grand.Core;
+using Grand.Framework.Components;
+using Grand.Web.Features.Models.Catalog;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Grand.Web.ViewComponents
+{
+    public class TopMenuViewComponent : BaseViewComponent
+    {
+        private readonly IMediator _mediator;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+
+        public TopMenuViewComponent(IMediator mediator,
+            IWorkContext workContext,
+            IStoreContext storeContext)
+        {
+            _mediator = mediator;
+            _workContext = workContext;
+            _storeContext = storeContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _mediator.Send(new GetTopMenu() {
+                Customer = _workContext.CurrentCustomer,
+                Language = _workContext.WorkingLanguage,
+                Store = _storeContext.CurrentStore
+            });
+
+            return View(model);
+        }
+    }
+}
