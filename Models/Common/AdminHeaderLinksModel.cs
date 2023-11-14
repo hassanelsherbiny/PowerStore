9c8d3f3becf3201e672cfbeb0af162ded8fc269b
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-11-14 10:06:14 +0200

    Added support for multiple payment methods (credit card, PayPal, etc)

---
+++ Models/Common/AdminHeaderLinksModel.cs
@@ -0,0 +1,12 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Common
+{
+    public partial class AdminHeaderLinksModel : BaseModel
+    {
+        public string ImpersonatedCustomerEmailUsername { get; set; }
+        public bool IsCustomerImpersonated { get; set; }
+        public bool DisplayAdminLink { get; set; }
+        public string EditPageUrl { get; set; }
+    }
+}
