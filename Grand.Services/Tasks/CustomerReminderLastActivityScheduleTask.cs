8ddca863221b7d37d72bc1da2abfda0ae99ac007
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-11-18 05:16:04 +0200

    Implemented schema markup

---
+++ Grand.Services/Tasks/CustomerReminderLastActivityScheduleTask.cs
@@ -0,0 +1,19 @@
+﻿using Grand.Services.Customers;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Tasks
+{
+    public partial class CustomerReminderLastActivityScheduleTask : IScheduleTask
+    {
+        private readonly ICustomerReminderService _customerReminderService;
+        public CustomerReminderLastActivityScheduleTask(ICustomerReminderService customerReminderService)
+        {
+            _customerReminderService = customerReminderService;
+        }
+
+        public async Task Execute()
+        {
+            await _customerReminderService.Task_LastActivity();
+        }
+    }
+}
