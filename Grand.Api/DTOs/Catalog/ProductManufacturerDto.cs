2c8317bdb5e45b9c110c9f1779578e9f78a2203a
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-12-20 03:02:04 +0200

    Implemented discount code functionality

---
+++ Grand.Api/DTOs/Catalog/ProductManufacturerDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grand.Api.DTOs.Catalog
+{
+    public partial class ProductManufacturerDto
+    {
+        [Key]
+        public string ManufacturerId { get; set; }
+        public bool IsFeaturedProduct { get; set; }
+    }
+}
