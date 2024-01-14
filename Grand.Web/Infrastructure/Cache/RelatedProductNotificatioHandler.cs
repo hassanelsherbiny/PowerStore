b9c3ce378cf17a6d9077c72deb74b6056fd56e14
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-01-14 13:46:08 +0200

    RTL Support #6

---
+++ Grand.Web/Infrastructure/Cache/RelatedProductNotificatioHandler.cs
@@ -0,0 +1,40 @@
+﻿using Grand.Core.Caching;
+using Grand.Domain.Catalog;
+using Grand.Core.Events;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Infrastructure.Cache
+{
+    public class RelatedProductNotificatioHandler :
+        INotificationHandler<EntityInserted<RelatedProduct>>,
+        INotificationHandler<EntityUpdated<RelatedProduct>>,
+        INotificationHandler<EntityDeleted<RelatedProduct>>
+    {
+
+        private readonly ICacheBase _cacheBase;
+
+        public RelatedProductNotificatioHandler(ICacheBase cacheManager)
+        {
+            _cacheBase = cacheManager;
+        }
+
+        public async Task Handle(EntityInserted<RelatedProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+
+        }
+        public async Task Handle(EntityUpdated<RelatedProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+        }
+        public async Task Handle(EntityDeleted<RelatedProduct> eventMessage, CancellationToken cancellationToken)
+        {
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId1));
+            await _cacheBase.RemoveByPrefix(string.Format(ModelCacheEventConst.PRODUCTS_RELATED_IDS_PATTERN_KEY, eventMessage.Entity.ProductId2));
+        }
+    }
+}
