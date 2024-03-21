b9a2cd6b69d4671357003a3be10eabb14f98df3c
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-03-21 02:54:06 +0200

    Clear cache event #172

---
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/Profiles/MeasureWeightProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Grand.Domain.Directory;
+using Grand.Core.Mapper;
+using Grand.Web.Areas.Admin.Models.Directory;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class MeasureWeightProfile : Profile, IAutoMapperProfile
+    {
+        public MeasureWeightProfile()
+        {
+            CreateMap<MeasureWeight, MeasureWeightModel>()
+                .ForMember(dest => dest.IsPrimaryWeight, mo => mo.Ignore());
+
+            CreateMap<MeasureWeightModel, MeasureWeight>()
+                .ForMember(dest => dest.Id, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
