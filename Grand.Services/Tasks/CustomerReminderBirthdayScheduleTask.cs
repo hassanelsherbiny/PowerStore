b79e5d903bf82af497469a7f4e87cd1f1c08c8b2
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-03-21 05:41:06 +0200

    CyberSource payment plugin. HTTP Signature Authentication update #177

---
+++ Grand.Services/Tasks/CustomerReminderBirthdayScheduleTask.cs
@@ -0,0 +1,20 @@
+﻿using Grand.Services.Customers;
+using System.Threading.Tasks;
+
+namespace Grand.Services.Tasks
+{
+    public partial class CustomerReminderBirthdayScheduleTask : IScheduleTask
+    {
+        private readonly ICustomerReminderService _customerReminderService;
+
+        public CustomerReminderBirthdayScheduleTask(ICustomerReminderService customerReminderService)
+        {
+            _customerReminderService = customerReminderService;
+        }
+
+        public async Task Execute()
+        {
+            await _customerReminderService.Task_Birthday();
+        }
+    }
+}
