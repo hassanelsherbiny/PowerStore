70446125821ea36823121d02b6c5ce2e0b0ea494
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-04-11 15:40:06 +0200

    Null reference Error, Admin/OrderModelFactory/PrepareOrderModelShippingInfoAsync #221

---
+++ Grand.Framework/Controllers/BasePaymentController.cs
@@ -0,0 +1,9 @@
+﻿namespace Grand.Framework.Controllers
+{
+    /// <summary>
+    /// Base controller for payment plugins
+    /// </summary>
+    public abstract class BasePaymentController : BasePluginController
+    {
+    }
+}
