d4d001c3ce20bfe98cd4a3ed66156a4e3c278c88
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-04-15 18:16:06 +0200

    Update deprecated packages #234

---
+++ Grand.Web/Models/Newsletter/SubscriptionActivationModel.cs
@@ -0,0 +1,9 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Web.Models.Newsletter
+{
+    public partial class SubscriptionActivationModel : BaseModel
+    {
+        public string Result { get; set; }
+    }
+}
