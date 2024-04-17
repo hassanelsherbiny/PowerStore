ced7f7157755bdd2917577823d5ee398afcc8359
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-04-17 12:50:06 +0200

    Use referrer url to find the correct breadcrumb instead of the first productcategory foun #262

---
+++ Grand.Web/Areas/Admin/Infrastructure/Mapper/Profiles/IWidgetPluginProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Grand.Core.Mapper;
+using Grand.Services.Cms;
+using Grand.Web.Areas.Admin.Models.Cms;
+
+namespace Grand.Web.Areas.Admin.Infrastructure.Mapper.Profiles
+{
+    public class IWidgetPluginProfile : Profile, IAutoMapperProfile
+    {
+        public IWidgetPluginProfile()
+        {
+            CreateMap<IWidgetPlugin, WidgetModel>()
+                .ForMember(dest => dest.FriendlyName, mo => mo.MapFrom(src => src.PluginDescriptor.FriendlyName))
+                .ForMember(dest => dest.SystemName, mo => mo.MapFrom(src => src.PluginDescriptor.SystemName))
+                .ForMember(dest => dest.DisplayOrder, mo => mo.MapFrom(src => src.PluginDescriptor.DisplayOrder))
+                .ForMember(dest => dest.IsActive, mo => mo.Ignore());
+        }
+
+        public int Order => 0;
+    }
+}
