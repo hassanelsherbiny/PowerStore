99dbc3b6e75567d62abe67d857abba99a7065a0e
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2024-04-21 03:37:06 +0200

    Forum permissions - Allow Customer to Create Post #289

---
+++ Grand.Web/Validators/Customer/RegisterValidator.cs
@@ -0,0 +1,126 @@
+﻿using FluentValidation;
+using Grand.Core;
+using Grand.Domain.Customers;
+using Grand.Core.Validators;
+using Grand.Services.Directory;
+using Grand.Services.Localization;
+using Grand.Web.Models.Customer;
+using System;
+using System.Collections.Generic;
+
+namespace Grand.Web.Validators.Customer
+{
+    public class RegisterValidator : BaseGrandValidator<RegisterModel>
+    {
+        public RegisterValidator(
+            IEnumerable<IValidatorConsumer<RegisterModel>> validators,
+            ILocalizationService localizationService,
+            IStateProvinceService stateProvinceService,
+            CustomerSettings customerSettings)
+            : base(validators)
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Email.Required"));
+            RuleFor(x => x.Email).EmailAddress().WithMessage(localizationService.GetResource("Common.WrongEmail"));
+
+
+            if (customerSettings.UsernamesEnabled)
+            {
+                RuleFor(x => x.Username).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Username.Required"));
+            }
+
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.FirstName.Required"));
+            RuleFor(x => x.LastName).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.LastName.Required"));
+
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Password.Required"));
+            RuleFor(x => x.Password).Length(customerSettings.PasswordMinLength, 999).WithMessage(string.Format(localizationService.GetResource("Account.Fields.Password.LengthValidation"), customerSettings.PasswordMinLength));
+            if (!string.IsNullOrEmpty(customerSettings.PasswordRegularExpression))
+                RuleFor(x => x.Password).Matches(customerSettings.PasswordRegularExpression).WithMessage(string.Format(localizationService.GetResource("Account.Fields.Password.Validation")));
+
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.ConfirmPassword.Required"));
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage(localizationService.GetResource("Account.Fields.Password.EnteredPasswordsDoNotMatch"));
+
+            //form fields
+            if (customerSettings.CountryEnabled && customerSettings.CountryRequired)
+            {
+                RuleFor(x => x.CountryId)
+                    .NotNull()
+                    .WithMessage(localizationService.GetResource("Address.Fields.Country.Required"));
+                RuleFor(x => x.CountryId)
+                    .NotEqual("")
+                    .WithMessage(localizationService.GetResource("Address.Fields.Country.Required"));
+            }
+            if (customerSettings.CountryEnabled &&
+                customerSettings.StateProvinceEnabled &&
+                customerSettings.StateProvinceRequired)
+            {
+                RuleFor(x => x.StateProvinceId).MustAsync(async (x, y, context) =>
+                {
+                    //does selected country has states?
+                    var countryId = !String.IsNullOrEmpty(x.CountryId) ? x.CountryId : "";
+                    var hasStates = (await stateProvinceService.GetStateProvincesByCountryId(countryId)).Count > 0;
+                    if (hasStates)
+                    {
+                        //if yes, then ensure that state is selected
+                        if (String.IsNullOrEmpty(y))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }).WithMessage(localizationService.GetResource("Account.Fields.StateProvince.Required"));
+            }
+            if (customerSettings.DateOfBirthEnabled && customerSettings.DateOfBirthRequired)
+            {
+                RuleFor(x => x.DateOfBirthDay).Must((x, context) =>
+                {
+                    var dateOfBirth = x.ParseDateOfBirth();
+                    if (!dateOfBirth.HasValue)
+                        return false;
+
+                    return true;
+                }).WithMessage(localizationService.GetResource("Account.Fields.DateOfBirth.Required"));
+
+                //minimum age
+                RuleFor(x => x.DateOfBirthDay).Must((x, context) =>
+                {
+                    var dateOfBirth = x.ParseDateOfBirth();
+                    if (dateOfBirth.HasValue && customerSettings.DateOfBirthMinimumAge.HasValue &&
+                        CommonHelper.GetDifferenceInYears(dateOfBirth.Value, DateTime.Today) <
+                        customerSettings.DateOfBirthMinimumAge.Value)
+                        return false;
+
+                    return true;
+                }).WithMessage(string.Format(localizationService.GetResource("Account.Fields.DateOfBirth.MinimumAge"), customerSettings.DateOfBirthMinimumAge));
+            }
+            if (customerSettings.CompanyRequired && customerSettings.CompanyEnabled)
+            {
+                RuleFor(x => x.Company).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Company.Required"));
+            }
+            if (customerSettings.StreetAddressRequired && customerSettings.StreetAddressEnabled)
+            {
+                RuleFor(x => x.StreetAddress).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.StreetAddress.Required"));
+            }
+            if (customerSettings.StreetAddress2Required && customerSettings.StreetAddress2Enabled)
+            {
+                RuleFor(x => x.StreetAddress2).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.StreetAddress2.Required"));
+            }
+            if (customerSettings.ZipPostalCodeRequired && customerSettings.ZipPostalCodeEnabled)
+            {
+                RuleFor(x => x.ZipPostalCode).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.ZipPostalCode.Required"));
+            }
+            if (customerSettings.CityRequired && customerSettings.CityEnabled)
+            {
+                RuleFor(x => x.City).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.City.Required"));
+            }
+            if (customerSettings.PhoneRequired && customerSettings.PhoneEnabled)
+            {
+                RuleFor(x => x.Phone).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Phone.Required"));
+            }
+            if (customerSettings.FaxRequired && customerSettings.FaxEnabled)
+            {
+                RuleFor(x => x.Fax).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Fax.Required"));
+            }
+        }
+    }
+}
